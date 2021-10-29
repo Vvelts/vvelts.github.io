cb28bc357a71ceb18f3650973ca96f48c0e2fee0
VeltsValentina <90384654+Vvelts@users.noreply.github.com>
2021-10-29 22:15:01 +0300

    Add files via upload

---
+++ web7/java7.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('.slider').slick({
+      arrows: true,
+      dots: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      speed:1000,
+      responsive:[
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        }
+      ]
+    });
+  });
